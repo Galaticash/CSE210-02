356c068fb31631dfb87225a460868020c4f1ce30
jlydikse <lyd15002@byui.edu>
2022-05-11 18:48:40 -0700

    Added a quick comment to prove that my system is set up correctly.

--- TeamF_Hilo.py
+++ TeamF_Hilo.py
@@ -1,3 +1,5 @@
 # Team Members
 # Ashley DeMott, Hailey Phipps, John Lydiksen
 # 
+
+# This is proof that I have set up the github stuff correctly and that I can push and pull the information as necessary.
